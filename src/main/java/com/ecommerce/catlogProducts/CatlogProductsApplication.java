d69fc8927bdf920b7bda4f2450b60b60157341b1
Harshalszz <477harshz@gmail.com>
2025-07-24 13:08:33 +0530

    Added models like product and catalog

---
+++ src/main/java/com/ecommerce/catlogProducts/CatlogProductsApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.catlogProducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatlogProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatlogProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/catlogProducts/CatlogProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.catlogProducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatlogProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
